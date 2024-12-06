96580b15ea5627bef174b5531cf37e7ae7d95e48
Thu Tran <chiujde6737@gmail.com>
2024-12-07 00:53:14 +0700

    trang thanh toan

--- routers/client/order.router.js
+++ routers/client/order.router.js
@@ -3,4 +3,5 @@ const router = express.Router();
 const controller = require("../../controllers/client/order.controller");
 router.get("/", controller.index);
 router.post("/", controller.orderPost);
+router.get("/success/:id", controller.success);
 module.exports = router;
